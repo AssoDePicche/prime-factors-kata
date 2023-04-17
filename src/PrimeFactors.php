ca65e1b79d4e1e0215a74227526e47d8eded1691
AssoDePicche <samuelprado730@gmail.com>
2023-04-17 06:09:13 -0300

    refactor: improve code design by reducing if statements

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -6,10 +6,8 @@ final class PrimeFactors
 {
     public function of(int $n): array
     {
-        if ($n === 2) {
-            return [2];
-        } else if ($n === 3) {
-            return [3];
+        if ($n > 1) {
+            return [$n];
         }
 
         return [];
