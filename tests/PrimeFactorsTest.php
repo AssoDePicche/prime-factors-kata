d7dba669131787c7bc427e14c8ccdbdb2a3c5e0b
AssoDePicche <samuelprado730@gmail.com>
2023-04-18 00:31:50 -0300

    style: standardize test names

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -11,32 +11,32 @@ final class PrimeFactorsTest extends \PHPUnit\Framework\TestCase
     $this->primeFactors = new PrimeFactors;
   }
 
-  public function test_one_should_return_an_empty_array(): void
+  public function test_prime_factors_of_one(): void
   {
     $this->assertSame([], $this->primeFactors(1));
   }
 
-  public function test_two_should_return_an_array_with_a_two(): void
+  public function test_prime_factors_of_two(): void
   {
     $this->assertSame([2], $this->primeFactors(2));
   }
 
-  public function test_three_should_return_an_array_with_a_three(): void
+  public function test_prime_factors_of_three(): void
   {
     $this->assertSame([3], $this->primeFactors(3));
   }
 
-  public function test_four_should_return_an_array_with_two_two(): void
+  public function test_prime_factors_of_four(): void
   {
     $this->assertSame([2, 2], $this->primeFactors(4));
   }
 
-  public function test_five_should_return_an_array_with_a_five(): void
+  public function test_prime_factors_of_five(): void
   {
     $this->assertSame([5], $this->primeFactors(5));
   }
 
-  public function test_six_should_return_an_array_with_a_two_and_a_three(): void
+  public function test_prime_factors_of_six(): void
   {
     $this->assertSame([2, 3], $this->primeFactors(6));
   }
